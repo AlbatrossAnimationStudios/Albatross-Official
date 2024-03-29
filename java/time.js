3938b7c656a07c7284cb6dd04532d51bc2ffd077
AlbatrossAnimationStudios <133553435+AlbatrossAnimationStudios@users.noreply.github.com>
2024-03-29 15:22:59 -0700

    Delete java/time.js

--- java/time.js
+++
@@ -1,5 +0,0 @@
-// code to update copyright year. by Mathias the Great!
-
-const currentYear = new Date().getFullYear();
-const copyrightElement = document.getElementById('copyrightyear');
-copyrightElement.textContent = `${currentYear} `;
